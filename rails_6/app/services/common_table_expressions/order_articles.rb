9682b62bd8ac1fae3526c8a0a9b44079011b2637
martinskruze <martins.kruze@gmail.com>
2021-12-26 13:05:13 +0200

    order atricles cte

--- rails_6/app/services/common_table_expressions/order_articles.rb
+++ rails_6/app/services/common_table_expressions/order_articles.rb
@@ -12,7 +12,7 @@ class CommonTableExpressions::OrderArticles
       # lets define new sql window function that can be used for getting correct row number
       longest_comment_window_function = Arel::Nodes::Window.new.
         partition(Comment.arel_table[:article_id]).
-        order(comment_length_function.desc)
+        order(Arel::Nodes::NamedFunction.new('length', [Comment.arel_table[:content]]).desc)
       # longest_comment_window_function.to_sql is:
       #   (PARTITION BY "comments"."article_id" ORDER BY length("comments"."content") DESC)
 
@@ -33,38 +33,69 @@ class CommonTableExpressions::OrderArticles
       #   SELECT
       #     "comments"."article_id",
       #     length("comments"."content") AS comment_length,
-      #     row_number() OVER (PARTITION BY "comments"."article_id" ORDER BY length("comments"."content") AS comment_length DESC) = 1 AS is_longest_comment
+      #     row_number() OVER (PARTITION BY "comments"."article_id" ORDER BY length("comments"."content") DESC) = 1 AS is_longest_comment
       #   FROM "comments"
-
 
       # lets define the CTE itself
       composed_longest_comments_cte = Arel::Nodes::As.new(
-        longest_comments_select,
-        longest_comments_cte
+        longest_comments_cte,
+        longest_comments_select
       )
       # longest_comments_select.to_sql is:
-      #   (
-      #     SELECT
-      #       "comments"."article_id",
-      #       length("comments"."content") AS comment_length,
-      #       row_number() OVER (PARTITION BY "comments"."article_id" ORDER BY length("comments"."content") AS comment_length DESC) = 1 AS is_longest_comment
-      #     FROM "comments"
-      #   ) AS "longest_comments"
+      # "longest_comments" AS (
+      #   SELECT
+      #     "comments"."article_id",
+      #     length("comments"."content") AS comment_length,
+      #     row_number() OVER (PARTITION BY "comments"."article_id" ORDER BY length("comments"."content") DESC) = 1 AS is_longest_comment
+      #   FROM "comments"
+      # )
+
+      # define our joining conditions
+      join_conditions = Article.arel_table[:id].
+        eq(longest_comments_cte[:article_id]).
+        and(longest_comments_cte[:is_longest_comment])
+      # join_conditions.to_sql is:
+      # "articles"."id" = "longest_comments"."article_id" AND "longest_comments"."is_longest_comment"
 
       # lets define the way articles will be joined to CTE
-      join_longest_comments_cte = Article.arel_table.join(longest_comments_cte).on(
-        Article.arel_table[:id].
-          eq(longest_comments_cte[:article_id]).
-          and(longest_comments_cte[:is_longest_comment])
-      ).join_sources
-      # join_longest_comments_cte.last.to_sql (this is array with one value) is:
-      #  INNER JOIN "longest_comments" ON "articles"."id" = "longest_comments"."article_id" AND "longest_comments"."is_longest_comment"
+      sql_statement = Article.arel_table.
+        project(Article.arel_table[Arel.star], longest_comments_cte[:comment_length]).
+        join(longest_comments_cte).
+        on(join_conditions).
+        order(longest_comments_cte[:comment_length].desc, Article.arel_table[:created_at].desc).
+        with(composed_longest_comments_cte)
+      # sql_statement.to_sql is:
+      # WITH "longest_comments" AS (
+      #   SELECT
+      #     "comments"."article_id",
+      #     length("comments"."content") AS comment_length,
+      #     row_number() OVER (
+      #       PARTITION BY "comments"."article_id"
+      #       ORDER BY
+      #         length("comments"."content") DESC
+      #     ) = 1 AS is_longest_comment
+      #   FROM
+      #     "comments"
+      # )
+      # SELECT
+      #   "articles".*,
+      #   "longest_comments"."comment_length"
+      # FROM "articles"
+      # INNER JOIN "longest_comments"
+      #   ON "articles"."id" = "longest_comments"."article_id"
+      #   AND "longest_comments"."is_longest_comment"
+      # ORDER BY
+      #   "longest_comments"."comment_length" DESC,
+      #   "articles"."created_at" DESC
 
-      # lets order articles with longes comment
-      Article.
-        with(composed_longest_comments_cte). #this does not work. why?
-        joins(join_longest_comments_cte).
-        order(longest_comments_cte[:comment_length].desc, Article.arel_table[:created_at].desc)
+      Article.find_by_sql(sql_statement)
+      # this finds all articles with their longest comment
+      # pp Article.find_by_sql(sql_statement).map { |a| [a.subject, a.comment_length].join(": ") }:
+      # ["Pills - do not take them!: 43",
+      #   "Arel is great: 26",
+      #   "Charge (1854): 20",
+      #   "The City of Sleep (1895): 20",
+      #   "If - (1910): 20"]
     end
   end
 end
